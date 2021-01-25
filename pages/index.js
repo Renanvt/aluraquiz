a611ca952130bb2785c4a5e9a267257f6195d05a
Renanvt <jonatan_rvs@hotmail.com>
2021-01-25 15:04:11 -0300

    adding db.json

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Title = styled.h1 `
+font-size: 50 px;
+color: $ {
+({ theme }) => theme.colors.secondary
+};
 `
 
+/*
+function Title(props) { //propcidades / propriedades
+    return (
+    <h1 >
+        {props.children} 
+    </h1>
+    )
+}
+*/
+
 export default function Home() {
-  return <Title>My page</Title>
+    return <Title> My page </Title>
 }
